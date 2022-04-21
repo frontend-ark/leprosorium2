a528c0d7ae087206af789e49e5ec1733fdb8095c
Yana Arkhipova <frontend.ark@gmail.com>
2022-04-21 19:05:21 +0600

    Add index erb

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 
 # обработчик get запроса /new
@@ -58,6 +58,8 @@ post '/new' do
     @error = 'Type post text'
     return erb :new
   end
+
+  # созранение данных в БД
 
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
